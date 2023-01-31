7040a02e8b4b977dded6396afee322a2e3c0cd57
RuslanUsmanov <usmanovruslan322@gmail.com>
2023-01-31 14:57:40 +0500

    add new function

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 def bool_to_str(var: bool):
     return str(var)
+
+
+def sum_list(list: list):
+    return sum(list)
--- test_main.py
+++ test_main.py
@@ -1,6 +1,10 @@
-from main import bool_to_str
+from main import bool_to_str, sum_list
 
 
 def test_bool_to_str():
     assert bool_to_str(True) == 'True'
     assert bool_to_str(False) == 'False'
+
+
+def test_sum():
+    assert sum_list([1, 2, 3]) == 6
