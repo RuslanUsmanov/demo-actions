50d6aafd2226ac1d17ccdc97996bf0b320e6f23d
RuslanUsmanov <usmanovruslan322@gmail.com>
2023-02-06 09:08:09 +0500

    update main

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ def bool_to_str(var: bool):
 
 def sum_list(list: list):
     return sum(list)
+
+
+def wirte_to_file():
+    with open('example.txt', 'w') as file:
+        file.write("hello world")
+
+
+if __name__ == "__main__":
+    wirte_to_file()
